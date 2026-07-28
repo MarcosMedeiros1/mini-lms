64bd231a0eadebd83426875fe8b41e298a665f1f
MarcosMedeiros1 <contato.marcosmedeiros01@gmail.com>
2026-07-28 06:17:14 -0300

    [#0303] Refactor: implement middleware support for request handling and add body parsing middleware

--- core/http/custom-req.ts
+++ core/http/custom-req.ts
@@ -12,20 +12,8 @@ export async function customReq(request: IncomingMessage) {
   const url = new URL(req.url || "", "http://localhost:3000");
   req.query = url.searchParams;
   req.pathname = url.pathname;
-  req.params = {}
-
-  const chunks: Buffer[] = [];
-  for await (const chunk of req) {
-    chunks.push(chunk);
-  }
-  const body = Buffer.concat(chunks).toString("utf-8");
-
-  if (req.headers["content-type"] === "application/json") {
-    const safeBody = body.trim();
-    req.body = safeBody ? JSON.parse(safeBody) : null;
-  } else {
-    req.body = {};
-  }
-
+  req.params = {};
+  req.body = {};
+  
   return req;
 }
