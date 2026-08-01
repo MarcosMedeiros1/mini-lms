18070518195e2e42645112e019b3667afeba45c8
MarcosMedeiros1 <contato.marcosmedeiros01@gmail.com>
2026-08-01 16:23:11 -0300

    [#0402] feat: implement course creation API and update client to post course data

--- api/lms/index.ts
+++ api/lms/index.ts
@@ -1,9 +1,38 @@
 import { Api } from "../../core/utils/abstract.ts";
+import { RouteError } from "../../core/utils/route-error.ts";
 import { lmsTables, lmsViews } from "./tables.ts";
 
 export class LmsApi extends Api {
+  handlers = {
+    postCourses: (req, res) => {
+      const { slug, title, description, lessons, hours } = req.body;
+      const writeResult = this.db
+        .query(
+          /*sql*/ `
+        INSERT OR IGNORE INTO 
+        "courses" ("slug", "title", "description", "lessons", "hours")
+        VALUES (?,?,?,?,?)
+      `,
+        )
+        .run(slug, title, description, lessons, hours);
+      console.log(writeResult);
+      if (writeResult.changes === 0) {
+        throw new RouteError(400, "Error on create course");
+      }
+      res.status(201).json({
+        id: writeResult.lastInsertRowid,
+        changes: writeResult.changes,
+        title: "Course created",
+      });
+    },
+  } satisfies Api["handlers"];
+
   tables(): void {
     this.db.exec(lmsTables);
     this.db.exec(lmsViews);
+  }
+
+  routes(): void {
+    this.router.post("/lms/courses", this.handlers.postCourses);
   }
 }
--- client.js
+++ client.js
@@ -2,8 +2,18 @@ const baseUrl = "http://localhost:3000";
 
 console.clear();
 const functions = {
-  async getCourse() {
-    const response = await fetch(baseUrl + "/course/javascript");
+  async postCourse() {
+    const response = await fetch(baseUrl + "/lms/courses", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        slug: "html-e-css",
+        title: "HTML e CSS",
+        description: "Curso de HTML e CSS para iniciantes",
+        lessons: 40,
+        hours: 10,
+      }),
+    });
     const body = await response.json();
     console.table(body);
   },
